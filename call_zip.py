ecbe0816c22b2212eadb375cd79ac48d8f5c1742
anandu-m-s <156334077+anandu-m-s@users.noreply.github.com>
2024-01-25 11:19:37 +0530

    Create call_zip.py

---
+++ call_zip.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+def download_zip(api_url):
+    response = requests.get(api_url, stream=True)
+
+    if response.status_code == 200:
+      destination_path = os.path.join(os.getenv("GITHUB_WORKSPACE"), os.path.basename("pkp.zip"))
+        with open(destination_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=128):
+                file.write(chunk)
+        print(f"ZIP file downloaded and saved to: {destination_path}")
+    else:
+        print(f"Failed to download ZIP file. Status code: {response.status_code}")
+
+if __name__ == "__main__":
+    # Set the URL of the API endpoint that provides the ZIP file
+    api_url = "https://www.free-css.com/assets/files/free-css-templates/download/page296/mediplus-lite.zip
+
+    download_zip(api_url)
