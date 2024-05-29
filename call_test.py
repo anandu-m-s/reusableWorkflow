da80444cac428244ac10364da7a04bcf82050ae4
anandu-ms <anandu.ms@hpe.com>
2024-05-30 01:56:22 +0530

    jfrog add

--- call_test.py
+++ call_test.py
@@ -1,8 +1,7 @@
-import requests
 import os
 import sys
 import zipfile
-def store_json_to_temp(json_file_path):
+def store_json_to_temp():
    try:
        workspace_path = os.getenv("GITHUB_WORKSPACE")
        destination_path = os.path.join(workspace_path, "tmp")
@@ -14,9 +13,7 @@ def store_json_to_temp(json_file_path):
             # set zip file path as output
        os.system("echo pkp_zip_path={} >> $GITHUB_OUTPUT".format(zip_file_path))   
 
-       print(f"JSON file '{json_file_path}' copied to workspace at: {destination_path}")
    except Exception as e:
         print(f"Error copying JSON file: {e}")    
 if __name__ == "__main__":
-   json_file_path = sys.argv[1]
-   store_json_to_temp(json_file_path)
+   store_json_to_temp()
